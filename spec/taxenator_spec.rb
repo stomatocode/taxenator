139964e3ee28bc9f3a6ef3fc92c422e846897e37
stomatocode <dewrgleision@gmail.com>
2014-03-11 23:34:16 -0700

    add notes to spec; stub module and class names

--- spec/taxenator_spec.rb
+++ spec/taxenator_spec.rb
@@ -14,14 +14,25 @@
 # Basic sales tax is applicable at a rate of 10% on all goods, except books, food, and 
 # medical products that are exempt. Import duty is an additional sales tax applicable on all 
 # imported goods at a rate of 5%, with no exemptions. 
+
+## module Import_duty
+## -> proc with rate application
+## module Tax_rate
+## -> proc with rate application
+
  
 # When I purchase items I receive a receipt, which lists the name of all the items and their 
 # price (including tax), finishing with the total cost of the items, and the total amounts of 
 # sales taxes paid. The rounding rules for sales tax are that for a tax rate of n%, a shelf 
 # price of p contains (np/100 rounded up to the nearest 0.05) amount of sales tax. 
+
+## from Basket to Printer
+## module Printer
  
 # Write an application that prints out the receipt details for these shopping baskets... 
 # INPUT: 
+
+## these inputs should come from a CSV as "scanned items", each a Basket object
  
 # Input 1: 
 # 1 book at 12.49 
@@ -63,6 +74,32 @@
 describe Printer do
   
 end
+
+describe Scanner do
+
+  let(:items) {Scanner.}
+
+  describe "#load_items" do
+    it "should read in items from a CSV (mock 'scanner')" do
+      expect
+    end
+  end
+
+end
+
+describe Basket do
+
+  context "good data" do
+
+  end
+
+  describe "#compute" do
+
+  end
+
+end
+
+
 
 
 
---
+++ taxenator.rb
@@ -0,0 +1,22 @@
+module Scanner
+
+end
+
+module Import_duty
+
+end
+
+module Tax_rate
+
+end
+
+module Printer
+
+end
+
+class Basket 
+  include Scanner
+  include Printer
+
+
+end
