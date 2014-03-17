28a49e837da9e5a840deb2b061b889ce08233a51
stomatocode <dewrgleision@gmail.com>
2014-03-17 04:01:58 -0700

    Print module; separate calculator logic

--- taxenator.rb
+++ taxenator.rb
@@ -2,21 +2,20 @@ require "csv"
 require "pry"
 
 module Ring_up
-  # adapt to accept ARGV for filename
   def self.run
-    baskets = []
-    if ARGV
-      ARGV.each do |file|
-        baskets << Basket.new(file)
-      end
+    if ARGV.empty?
+      basket = Basket.new("items.csv")
+      basket.compute
+      basket.print
     else
-      basket = Basket.new(filename)
+      ARGV.each do |file|
+        basket = Basket.new(file)      
+        basket.compute
+        basket.print
+      end
     end
-
   end
-
 end
-
 
 module Scanner
   def load_items_csv(filename)
@@ -25,7 +24,7 @@ module Scanner
       items[row[:name].split(/ /).first.downcase.to_sym] = row.to_hash
     end
     items.each_value do |item|
-      item[:type] = item[:type].to_sym
+      item[:type]   = item[:type].to_sym
       item[:origin] = item[:origin].to_sym
     end
     items
@@ -33,16 +32,15 @@ module Scanner
 end
 
 module Printer
-  def print
+  def print_totals
     puts  "Pre-tax total: #{@pre_tax_total}"
     puts  "Tax: #{@tax}"
     puts  "Final: #{@final}"
   end
 
-# def purchase_price
-#   sprintf( "%.2f",attributes['purchase_price'])
-# end
-
+  def print_items(item)
+    puts "#{item[:quantity]} #{}"
+  end
 end
 
 class Basket 
@@ -60,16 +58,16 @@ class Basket
 
   def compute
     pre_tax
-    p @pre_tax_total
     @taxenator = Tax_calculator.new
     @taxenator.calculate(@items)
     @items.each_value {|post_tax| @final += post_tax[:price]}
     @tax = @final - @pre_tax_total
-    p "fart #{@tax.round(2)}"
+    @tax = @tax.round(2)
   end
 
   def pre_tax
     @items.each_value {|pre_tax| @pre_tax_total += pre_tax[:price]}    
+    @pre_tax_total = @pre_tax_total.round(2)
   end
 
 end
@@ -85,42 +83,43 @@ class Tax_calculator
   def calculate(items)
     items.each_value do |item|
       if item[:origin] == :for
-        if !@tax_exempt.include?(item[:type])
-          item[:price] = item[:price] + (item[:price] * @tax_rate) + (item[:price] * @import_duty)
-          item[:price] = item[:price].round(2)
-          p "foreign not exempt: #{item[:price]}"
-        end
-        item[:price] = item[:price] + (item[:price] * @import_duty)
-        item[:price] = item[:price].round(2)
-        p "foreign exempt #{item[:price]}"
+        foreign(item)        
       else
-        if !@tax_exempt.include?(item[:type])
-          item[:price] = item[:price] + (item[:price] * @tax_rate)
-          item[:price] = item[:price].round(2)
-          p "domestic not exempt: #{item[:price]}"
-        end
-
-        item[:price]
-        p "domestic exempt: #{item[:price]}"
+        domestic(item)
       end
-    item[:price] = item[:price].round(2)
     end
   end
 
-  def round=(num) 
-    num=num.round(2)
+  def foreign(item)
+    if exempt?(item[:type])
+      item[:price] = item[:price] + (item[:price] * @import_duty)
+      item[:price] = item[:price].round(2)
+      p "foreign exempt #{item[:price]}"
+    else
+      item[:price] = item[:price] + (item[:price] * @tax_rate) + (item[:price] * @import_duty)
+      item[:price] = item[:price].round(2)
+      p "foreign not exempt: #{item[:price]}"
+    end   
   end
 
-  def imported
-    
+  def domestic(item)
+    if exempt?(item[:type])
+      item[:price]
+      p "foreign exempt #{item[:price]}"
+    else
+      item[:price] = item[:price] + (item[:price] * @tax_rate)
+      item[:price] = item[:price].round(2)
+      p "foreign not exempt: #{item[:price]}"
+    end 
   end
 
-  def domestic
-    
+  def exempt?(type)
+    @tax_exempt.include?(type)
   end
 
 end
 
-basket = Basket.new("input3.csv")
-binding.pry
-basket.compute
+# basket = Basket.new("input3.csv")
+Ring_up.run
+# binding.pry
+# basket.compute
